05781728a4bd6254914f88071c7a56ea3f7e8c10
waelabohamza <waeleagle1243@gmail.com>
2021-01-15 18:38:22 +0200

    refresh

--- lib/const.dart
+++ lib/const.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const serverName = "192.168.1.2:8080/yaser" ;  
-// const serverName = "almutajaralarabi.com/api" ;  
+// const serverName = "192.168.1.2:8080/yaser" ;  
+const serverName = "almutajaralarabi.com/api" ;  
 
 const Color maincolor = Color(0xFF3498db); 
 
