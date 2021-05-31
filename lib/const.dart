bed292af980293f7c5ee658667ad1fd9a9dd7b32
waelabohamza <waeleagle1243@gmail.com>
2021-05-31 12:11:50 +0300

    refresh

--- lib/const.dart
+++ lib/const.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-// const serverName = "192.168.1.2:8080/yaser" ;  
-const serverName = "almutajaralarabi.com/api" ;  
+const serverName = "10.0.2.2:8080/apicommercealarabi" ;  
+// const serverName = "almutajaralarabi.com/api" ;  
 // const serverName = "talabgo.com/yaser" ;  
 // const serverName = "almotorkw.com/mutajar" ;  
 
